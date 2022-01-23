518bb139764b4512877ad106f01e78ad59c4936d
Tanvi C <tanvichhabra14@gmail.com>
2022-01-23 14:31:20 -0500

    Update lilypad_if.ino

--- PianoCode1/lilypad_if.ino
+++ PianoCode1/lilypad_if.ino
@@ -1,13 +1,19 @@
 // sensor keys
-int key1 = 11;
-int key2 = A2;
-int key3 = A3;
-int key4 = A4;
-
+int key1 = 6;
+int key2 = 9;
+int key3 = 10;
+int key4 = 11;
+int key5 = A2;
+int key6 = A3;
+int key7 = A4;
 int speaker = 5;                 // name of the speaker key
-int sensorValue;                  // will store sensor readings
+int touchValue;                  // will store sensor readings
 
 //musical notes
+int C = 1046;
+int D = 1175;
+int E = 1319;
+int F = 1397;
 int G = 1598;
 int A = 1760;
 int B = 1976;
@@ -20,33 +26,33 @@ void setup() {
     pinMode(key2, INPUT);
     pinMode(key3, INPUT);
     pinMode(key4, INPUT);
+    pinMode(key5, INPUT);
+    pinMode(key6, INPUT);
+    pinMode(key7, INPUT);
     pinMode(speaker, OUTPUT);   // set speaker to be an output
     Serial.begin(9600);         // initialize the communication
 }
 
 void loop() {
-    checkPianoKey(key1, A);
-    checkPianoKey(key2, B);
-    checkPianoKey(key3, C1);
-    checkPianoKey(key4, D1);
+    checkPianoKey(key1, C);
+    checkPianoKey(key2, D);
+    checkPianoKey(key3, E);
+    checkPianoKey(key4, G);
+    checkPianoKey(key5, A);
+    checkPianoKey(key6, C1);
+    checkPianoKey(key7, D1);
     delay(100);
 }
 
 void checkPianoKey (int key, int note)
 {
-    sensorValue = analogRead(key); // read the touch sensor value
-    Serial.println(sensorValue);          // send touchValue to computer
-    
-    if (sensorValue == 0)                  // if the key is pressed
+    touchValue = readCapacitivePin(key); // read the touch sensor value
+    Serial.println(touchValue);          // send touchValue to computer
+    if (touchValue > 1)                  // if the key is pressed
     {
-        tone(speaker, note, 1000);             // play a note
-       
-        
-       delay(1500);
-        
-        
+        tone(speaker, note);             // play a note
+        delay(100);                      // wait for 1/10th of a second
     }
-   
     else                                 // if the key is not pressed
     {
         noTone(speaker);                 // stop playing the note
